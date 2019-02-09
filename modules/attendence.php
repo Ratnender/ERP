25b7790049b4179ac1199bdadf81c549214fad3d
0001SID <mailofsumandas@gmail.com>
2019-02-09 13:34:29 +0530

    linking date and section

--- modules/attendence.php
+++ modules/attendence.php
@@ -16,25 +16,28 @@
 			$temp_p_date = strtotime($p_date);
 			$p_date = date('Ymd',$temp_p_date);
 			$date_test = "select * from attendence where l_date =".$p_date.";";
-			$count = 0;
+			$count = 0;$btn_check= 0;
 			if($result = $conn->query($date_test))
 			{
-				$id_no = 1;
+				$id_no = $_POST['first_id'];
 				$get_date = $result->fetch_assoc();
 				$test_date = $get_date['l_date'];
 				if(!is_null($test_date))
 				{
+					$btn_check = 0;
 					while (TRUE)
 					{
 						$attend = $_POST[$id_no];
-						$tem_query = "update attendence set attendence ='".$attend."'where id =".$id_no." and l_date=".$p_date.";";
+						$temp = "r_".$id_no;
+						$remark = $_POST[$temp];
+						$tem_query = "update attendence set attendence ='".$attend."',remark = '".$remark."' where id =".$id_no." and l_date=".$p_date.";";
 						if($attend == NULL) break;
 						if($conn->query($tem_query) === TRUE)
 						{
 							$count = 1;
 						}
 						else{
-							$count = 2;
+							echo 'Update failed '.$conn->error;
 						}
 						$id_no++;
 
@@ -43,10 +46,13 @@
 			
 				else
 				{
+					$btn_check = 1;
 					while (TRUE)
 					{
 						$attend = $_POST[$id_no];
-						$tem_query = "insert into attendence values(".$id_no.",".$p_date.",(select name from students where id =".$id_no."),'".$attend."');";
+						$temp = "r_".$id_no;
+						$remark = $_POST[$temp];
+						$tem_query = "insert into attendence values(".$id_no.",".$p_date.",(select name from students where id =".$id_no."),'".$attend."','".$remark."');";
 						// echo $attend.'  attend'.$id_no.'<br>';
 						if($attend == NULL) break;
 						// echo '<br><br>'.$tem_query.'<br><br>';
@@ -55,7 +61,7 @@
 							$count = 3;
 						}
 						else{
-							$count = 4;
+							echo 'new Data entry failed '.$conn->error;
 						}
 						$id_no++;
 
@@ -65,20 +71,39 @@
 			
 		}
 	?>
-	<form action =<?php echo $_SERVER['PHP_SELF']?>  method = "post">
 	<div class = "choice_area">
+	<form action = <?php echo $_SERVER['PHP_SELF']?> method = "post" id = "first_form">
+
+	<?php
+		$sec = $_POST['section'];
+		if(is_null($sec)){
+			$sec = "A1";
+		}
+
+		$date_value = $_POST['date'];
+		if(is_null($date_value)){
+		$date_value = date("Y-m-d");
+		}
+				
+		?>
 		Enter Date:
-		<input class = "choice_box" type = "date" name = 'date' value = "<?php echo date("Y-m-d")?>"/>
+		<input class = "choice_box" type = "date" name = 'date' value = "<?php echo $date_value; ?>"/>
 		Section:
-		<select class = "choice_box">
-			<option name = "section" value ="A1">A1</option>
-			<option name = "section" value ="A2">A2</option>
-			<option name = "section" value ="B1">B1</option>
-			<option name = "section" value ="B2">B2</option>
-			<option name = "section" value ="C1">C1</option>
-			<option name = "section" value ="C2">C2</option>
+		<select id = "section" name = "section" onchange = "change_sec()" class = "choice_box">
+			<option value ="A1"<?php if ($sec == "A1"){ echo 'selected';}?>>A1</option>
+			<option value ="A2"<?php if ($sec == "A2"){ echo 'selected';}?>>A2</option>
+			<option value ="B1"<?php if ($sec == "B1"){ echo 'selected';}?>>B1</option>
+			<option value ="B2"<?php if ($sec == "B2"){ echo 'selected';}?>>B2</option>
+			<option value ="C1"<?php if ($sec == "C1"){ echo 'selected';}?>>C1</option>
+			<option value ="C2"<?php if ($sec == "C2"){ echo 'selected';}?>>C2</option>
 			</select>
+			<input class = "btn" type = "submit" name = "search" value = "Search"/>
 	</div>
+	</form>
+	<form action =<?php echo $_SERVER['PHP_SELF']?>  method = "post">
+	<!-- <form action = "../php/test.php" method = "post"> -->
+	<input type = 'hidden' name = 'date' value = "<?php if (isset($_POST['search'])){echo $_POST['date'];} else {echo date("Y-m-d");}?>"/>
+	<input type = 'hidden' name = 'section' value = "<?php if (isset($_POST['search'])){echo $_POST['section'];} else {echo "A1";}?>"/>
 		<table>
 			<tr>
 				<th>Name</th>
@@ -87,59 +112,59 @@
 				<th>Absent</th>
 				<th>Madical</th>
 				<th>On Leave</th>
-				<th>Other</th>
+				<th>Remark</th>
 			</tr>
 		<?php
-		$student_query = "select id,name,roll_no from students;";
+		if(isset($_POST['search'])){
+		$sec = $_POST['section'];}
+		else
+		{$sec = "A1";}
+		$student_query = 'select id,name,upper(college_roll) as college_roll,unv_roll from students where sec_group = "'.$sec.'";';
 		if($result = $conn->query($student_query))
 		{
+			$loop_count = 1;
 			while ($row = $result->fetch_assoc())
 			{
 
 				$name = $row['name'];
-				$roll = $row['roll_no'];
+				$college_roll = $row['college_roll'];
+				$unv_roll = $row['unv_roll'];
 				$id = $row['id'];
+
+				if($loop_count == 1)
+				{
+					echo '<input type = "hidden" name = "first_id" value = "'.$id.'"/>';
+					$loop_count+=1;
+					$loop_count++;
+				}
 				?>
-				<tr <?php if($id%2 ==0) {echo "class = 'odd'";}else{echo "class = 'even'";}?>>
+				<tr <?php if($id%2 ==0) {echo "class = 'even'";}else{echo "class = 'odd'";}?>>
 					<td><?php echo $name ?></td>
-					<td><?php echo $roll ?></td>
-					<td style = "height:1em;weidth:1em"><input type = "radio" name ="<?php echo $id?>" value = "present" checked = "checked"/></td>
-					<td style = "height:1em;weidth:1em"><input type = "radio" name ="<?php echo $id?>" value = "absent"/></td>
-					<td style = "height:1em;weidth:1em"><input type = "radio" name ="<?php echo $id?>" value = "medical"/></td>
-					<td style = "height:1em;weidth:1em"><input type = "radio" name ="<?php echo $id?>" value = "on_leave"/></td>
-					<td><input class = "text-box" type = "text" name ="an_<?php echo $id?>" value = "" placeholder = "Another reason"/></td>
+					<td><?php echo ($college_roll).' / '.$unv_roll ?></td>
+					<td><input type = "radio" name ="<?php echo $id?>" value = "present" checked = "checked"/></td>
+					<td><input type = "radio" name ="<?php echo $id?>" value = "absent"/></td>
+					<td><input type = "radio" name ="<?php echo $id?>" value = "medical"/></td>
+					<td><input type = "radio" name ="<?php echo $id?>" value = "on_leave"/></td>
+					<td><input class = "text-box" type = "text" name ="r_<?php echo $id?>" value = "" placeholder = "Not listed reason"/></td>
 				</tr>
 			<?php
 			}?>
+			<p id = 'test'></p>
 		</table>
 		<input id = "attendence_reset" class = "btn" type = "reset" name = "reset" value = "Reset"/>
-		<input id = "attendence_submit" class = "btn" type = "submit" name = "submit" value = "Save"/>
+		<input id = "attendence_submit" onclick = "firstClick()" class = "btn" type = "submit" name = "submit" value = "Save"/>
 	</form>
 		<?php
 		}
 		else{
-			echo 'Query failed'.$conn->error;
+			echo 'Query failed Database not active'.$conn->error;
 		}
 		?>
 <?php include "../php/end.php" ?>
+<script src = "../javascript/attendence.js"></script>
 <script>
+	// ck_btn = <?php echo $btn_check ?>;
+	// check_btn(ck_btn);
 	count = <?php echo $count ?>;
-	switch(count)
-	{
-		case 0:
-		window.alert('Query Failed');
-		break;
-		case 1:
-		window.alert('Update Successfull');
-		break;
-		case 2:
-		windwo.alert('Update Failed');
-		break;
-		case 3:
-		window.alert('New Entry added');
-		break;
-		case 4:
-		window.alert('New Entry Failed');
-		break;
-	}
+	check_update_popup(count);
 </script>
