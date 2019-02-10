53482ae517fcb082bb6ab26ec43c576cccb1ed7f
0001SID <mailofsumandas@gmail.com>
2019-02-10 11:30:54 +0530

    little column change in create subject table

--- php/functions.php
+++ php/functions.php
@@ -1,4 +1,5 @@
 <?php
+    require "connection.php";
     function check_connection()
         {
             if($conn->connect_error)
@@ -25,11 +26,39 @@
         $query = "create table $name
         (
             id int not null primary key,
-            lec_date date not null,
+            l_date date not null,
             name varchar(40) not null,
             attendance varchar(10) not null
         );";
         exec_query($query);
     }
+    
+    // function check_date($p_date,$table,$column)
+    // {
+    //     $temp_p_date = strtotime($p_date);
+    //     $p_date = date('Ymd',$temp_p_date);
+    //     $date_test = "select * from ".$table." where ".$column." =".$p_date.";";
+    //     echo 'in function';
+    //     echo '<br>'.$date_test;
+    //     echo 'testing'.$conn->query($date_test);
+    //     if($result = $conn->query($date_test))
+    //     {
+    //         echo 'in if';
+    //         $get_date = $result->fetch_assoc();
+    //         $test_date = $get_date[$column];
+    //         if(!is_null($test_date))
+    //         {
+    //             $res = NULL;
+    //         }
+    //         else{
+    //             $res = $p_date;
+    //         }
+    //         return $res;
+    //     }
+    //     else
+    //     {
+    //         echo 'function failed'.$conn->error;
+    //     }
+    // }
 
 ?>
