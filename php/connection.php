fe06cc6fac557088877ec8513c0ad81daf04b5f1
0001SID <mailofsumandas@gmail.com>
2019-02-19 19:42:19 +0530

    rm

--- php/connection.php
+++ php/connection.php
@@ -8,14 +8,6 @@ switch($service){
         $host = "localhost";
         $database = "erp";
         break;
-// Amazon database
-    case 'aws':
-        $username = "siddb";
-        $password = "siddb9091";
-        $host = "sidsql.c16xhakp74ff.us-east-2.rds.amazonaws.com";
-        $database = "erp";
-        break;
-
 // Google database
     case 'google':
         $username = "sid";
@@ -23,14 +15,6 @@ switch($service){
         $host = "35.202.253.161";
         $database = "erp";
         break;
-    
-//db4free database
-    case 'db4free':
-        $username = "sidsql";
-        $password = "sql@9091";
-        $host = "db4free.net";
-        $database = "erptest";
-        break;
-    }
+}
     $conn = new mysqli($host,$username,$password,$database);
 ?>
