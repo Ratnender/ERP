013f2fe7c90edd009a65822e4b15fe359aa88a0e
0001SID <mailofsumandas@gmail.com>
2019-02-19 15:02:28 +0530

    added and activated google database

--- php/connection.php
+++ php/connection.php
@@ -1,8 +1,17 @@
 <?php
+//Amazon database
+
+    // $username = "siddb";
+    // $password = "siddb9091";
+    // $host = "sidsql.c16xhakp74ff.us-east-2.rds.amazonaws.com";
+    // $database = "erp";
+    // $conn = new mysqli($host,$username,$password,$database);
+
+//Google database
+    
     $username = "siddb";
     $password = "siddb9091";
-    $host = "sidsql.c16xhakp74ff.us-east-2.rds.amazonaws.com";
+    $host = "35.202.253.161";
     $database = "erp";
     $conn = new mysqli($host,$username,$password,$database);
-   
 ?>
